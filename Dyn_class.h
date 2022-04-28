b2b79fdc1bf26a8522db0267b1f68b6d2f78c410
darejester <53415886+darejester@users.noreply.github.com>
2022-04-28 17:04:05 -0400

    Update Dyn_class.h

--- Dyn_class.h
+++ Dyn_class.h
@@ -1,12 +1,19 @@
 #pragma once
+#include"preCompiled.h"
 
 
 
 class Dyn_class {
 public: 
-	Dyn_class(); //TODO: add param f
-	void remove_func(); //TODO: add param f
-	void add_func(); //TODO: add param f 
+	Dyn_class(); 
+	void remove_func(); 
+	void add_func(); 
 
 private:
+	// DATA STRUCTURE
+	////////////////////JOHN's proposal ///////////////////
+	//std:: map< string, vector<any> > storage; 
+	
+
+
 };
