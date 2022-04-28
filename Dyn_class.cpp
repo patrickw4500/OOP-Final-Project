a61ae74e5eb512547b92068f9c5b00d7e7fdb549
darejester <53415886+darejester@users.noreply.github.com>
2022-04-28 17:03:31 -0400

    Update Dyn_class.cpp

--- Dyn_class.cpp
+++ Dyn_class.cpp
@@ -1,16 +1,16 @@
 #include "Dyn_class.h"
-#include <iostream>
-using namespace std;
-
 
 Dyn_class::Dyn_class() {
 	cout << "must implement Dynclass ctor" << endl;
-}//TODO: add param f
+	
+}
+
 void Dyn_class::remove_func() {
 	cout << "must implement Dynclass remove_func" << endl;
-}//TODO: add param f
+}
+
 void Dyn_class::add_func() {
 	cout << "must implement Dynclass add_func" << endl;
 
-}//TODO: add param f 
+}
 
