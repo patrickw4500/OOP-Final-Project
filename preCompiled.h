4cb17697538ca338b84cb89466c7383a485ce8d3
darejester <53415886+darejester@users.noreply.github.com>
2022-05-02 20:35:48 -0400

    Update preCompiled.h

--- preCompiled.h
+++ preCompiled.h
@@ -1,8 +1,14 @@
 #pragma once
-using namespace std; // for std
+
+
 #include <map> // maps
 #include <functional> // for functional and bind
 #include <iostream> // input/output
 #include <vector> // vector
 #include <any> // any
 #include <iomanip> // spacing (ex. setw)
+
+using namespace std; // for std
+using namespace placeholders;
+using Plus = std::plus<int>;
+using Minus = std::minus<int>;
